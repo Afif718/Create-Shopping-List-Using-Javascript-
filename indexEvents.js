cfa376d610a98b8655e62cef2d0027038ab2d423
Afif718 <39927237+Afif718@users.noreply.github.com>
2021-07-25 02:12:10 +0600

    Add files via upload

---
+++ indexEvents.js
@@ -0,0 +1,14 @@
+var button = document.getElementsByTagName("button")[0];
+
+// button.addEventListener("click", function(){
+// 	console.log("Click!!")
+// })
+
+button.addEventListener("mouseenter", function(){
+	console.log("Click!!")
+});
+
+
+// button.addEventListener("mouseleave", function(){
+// 	console.log("Click!!")
+// });
